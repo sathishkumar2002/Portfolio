43c1684a3072c8ffdf4d5e61f2a8ae3a3828d20b
sathishkumar2002 <skumar3072002@gmail.com>
2025-05-18 13:31:29 +0530

    updated the app style

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,15 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="font-sans text-gray-800 bg-gradient-to-br from-gray-100 to-blue-50 min-h-screen">
+    <div className="font-sans text-gray-800 bg-gray-50 min-h-screen">
+      {/* Removed header here */}
       <main className="space-y-12">
-        <About />
-        <hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
-        <Skills />
-        <hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
-        <Experience />
-        <hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
-        <Certifications />
-        <hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
-        <Contact />
-        <hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
+        <About /><hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
+        <Skills /><hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
+        <Experience /><hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
+        <Certifications /><hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
+        <Contact /><hr className="border-t border-gray-200 my-12 w-3/4 mx-auto" />
+
       </main>
       <footer className="p-4 bg-gray-100 text-center text-sm text-gray-500">
         © 2025 Sathish Kumar
@@ -26,3 +23,5 @@ function App() {
     </div>
   );
 }
+
+export default App;
